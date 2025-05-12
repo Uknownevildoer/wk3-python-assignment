788a7065c6daba894bb5ddc4bffba9c71a4c3d15
Uknownevildoer <kithiiajames@gmail.com>
2025-05-13 00:08:09 +0300

    Update WK3.py

--- WK3.py
+++ WK3.py
@@ -1,1 +1,11 @@
-
+#QUESTION 1
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20 and discount_percent<100 :
+        price = price-(discount_percent/100 * price)
+        return price
+    else:
+        return price
+#QUESTION 2
+price= int(input("Enter original price: "))
+discount_percent= float(input("Enter discount percentage: ")) 
+print(calculate_discount(price,discount_percent))
